dd99472815bc07fbe0388119dfcb581524d844d8
Ryan Zwiefelhofer <ryan.zwiefelhofer@connexta.com>
2020-12-02 10:20:55 -0700

    cleaned up to make it a little more presentable

--- src/aoc_2020.clj
+++ src/aoc_2020.clj
@@ -17,7 +17,7 @@
 
 ; --------------------------------------------------------
 ; Day 1 Problem 1
-(def nums (file->numvec "2020-d1-p1.txt"))
+(def d1-input (file->numvec "2020-d1-p1.txt"))
 
 (defn find-2020-num [lines]
   (let [remain (set (map #(- 2020 %) lines))]
@@ -35,7 +35,7 @@
   (is (= (find-2020-num [1 2 3 4 5 1900 120]) 1900)))
 
 (deftest expense-product
-  (is (= (find-2020-prod nums) 1010299)))
+  (is (= (find-2020-prod d1-input) 1010299)))
 
 ; Day 1 Problem 2
 
@@ -46,12 +46,17 @@
       (c/combinations lines 3))))
 
 (deftest test-2020-trips
-  (is (= (find-2020-trip nums) '(1488 60 472)))
-  (is (= (apply * (find-2020-trip nums)) 42140160)))
+  (is (= (find-2020-trip d1-input) '(1488 60 472)))
+  (is (= (apply * (find-2020-trip d1-input)) 42140160)))
 
 
 ; --------------------------------------------------------
 ; Day 2 Part 1
+(def day2-input
+  (->> "2020-d2.txt"
+       file->vec
+       parts))
+
 (defn parts [lines]
   (map #(s/split % #" ") lines))
 
@@ -67,9 +72,7 @@
     (and (>= let-count min) (<= let-count max))))
 
 (defn count-valid-sled []
-  (->> "2020-d2.txt"
-       file->vec
-       parts
+  (->> day2-input
        (map check-sled-pass)
        (filter true?)
        count))
@@ -95,9 +98,7 @@
     (or (and p1? (not p2?)) (and p2? (not p1?)))))
 
 (defn count-toboggan-valid []
-  (->> "2020-d2.txt"
-       file->vec
-       parts
+  (->> day2-input
        (map check-toboggan-pass)
        (filter true?)
        count))
