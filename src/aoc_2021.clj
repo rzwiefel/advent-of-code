2eb4a358a8d21aadb0a7db2086d25541907f2ad6
Ryan Zwiefelhofer <rzwiefelhofer@guaranteedrate.com>
2021-12-02 12:30:04 -0700

    d2

--- src/aoc_2021.clj
+++ src/aoc_2021.clj
@@ -71,3 +71,61 @@
 
 (deftest d1p2
   (is (= 1805 (calc-deltas (calc-3-window d1-input)))))
+
+
+; --------------------------------------------------------
+; Day 2
+
+(def d2-input (file->vec "2021-d2.txt"))
+(def d2-test (s/split-lines "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"))
+
+(defn parse-substructions
+  [input-vec]
+  (for [line input-vec
+        :let [[_ dir amount] (re-find #"(\w+)\s(\d)" line)]]
+    [(keyword dir) (Integer/parseInt amount)]))
+
+(defn calculate-sub-position
+  [substructions]
+  (reduce (fn [acc [dir amount]]
+            (update acc dir + amount))
+          {:forward 0 :down 0 :up 0}
+          substructions))
+
+(deftest d2p1
+  (is (= 150
+         (let [{:keys [forward down up]} (calculate-sub-position
+                                          (parse-substructions d2-test))]
+           (* forward (- down up)))))
+  (is (= 1524750
+         (let [{:keys [forward down up]} (calculate-sub-position
+                                          (parse-substructions d2-input))]
+           (* forward (- down up))))))
+
+(defn calculate-sub-position-aimed
+  [substructions]
+  (reduce (fn [acc [dir amount]]
+            #_(tap> {:acc acc :dir dir :amount amount})
+            (cond
+              (= dir :down)    (update acc :aim + amount)
+              (= dir :up)      (update acc :aim - amount)
+              (= dir :forward) (-> acc
+                                   (update :forward + amount)
+                                   (update :depth + (* (:aim acc) amount)))))
+          {:forward 0 :aim 0 :depth 0}
+          substructions))
+
+(deftest d2p1
+  (is (= 900
+         (let [{:keys [depth forward]} (calculate-sub-position-aimed
+                                          (parse-substructions d2-test))]
+           (* depth forward))))
+  (is (= 1592426537
+         (let [{:keys [depth forward]} (calculate-sub-position-aimed
+                                          (parse-substructions d2-input))]
+           (* depth forward)))))
